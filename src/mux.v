9abd8444cc459ad658609173243dbc2d1497a927
cjayross <calvinjayross@gmail.com>
2019-09-19 21:09:14 -0500

    gave mux variable data width

--- src/mux.v
+++ src/mux.v
@@ -1,15 +1,17 @@
 module mux (out, A, B, S);
 
-output wire [15:0] out;
-input wire [15:0] A, B;
+parameter WIDTH = 16;
+
+output wire [WIDTH-1:0] out;
+input wire [WIDTH-1:0] A, B;
 input wire S;
 
 wire _S;
-wire [15:0] bus1, bus2;
+wire [WIDTH-1:0] bus1, bus2;
 
 not (_S, S);
-and u1 [15:0] (bus1[15:0], {16{_S}}, A[15:0]);
-and u2 [15:0] (bus2[15:0], {16{S}}, B[15:0]);
-or u3 [15:0] (out[15:0], bus1[15:0], bus2[15:0]);
+and u1 [WIDTH-1:0] (bus1[WIDTH-1:0], {WIDTH{_S}}, A[WIDTH-1:0]);
+and u2 [WIDTH-1:0] (bus2[WIDTH-1:0], {WIDTH{S}}, B[WIDTH-1:0]);
+or u3 [WIDTH-1:0] (out[WIDTH-1:0], bus1[WIDTH-1:0], bus2[WIDTH-1:0]);
 
 endmodule
